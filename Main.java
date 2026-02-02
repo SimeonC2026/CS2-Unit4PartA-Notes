6f10068fc0a7557ba879f50fb352fa7aae01eed9
SimeonC2026 <simeon.chew2026@gbwl.org>
2026-02-02 14:33:01 +0000

    4.3 Notes

--- Main.java
+++ Main.java
@@ -14,10 +14,33 @@ public class Main {
       boolean[] yesNo = new boolean[3]; // default values: false
 
       // How to ACCESS an item in an Array
-      System.out.println( prices[0] );
-      
+      // arrayName[i] -> i is the INDEX (position)
+      System.out.println( prices[0] ); // FIRST ITEM!
 
+      // Use the same syntax to SET a value in an Array
+      luckyNums[0] = 13; 
+      luckyNums[5] = 7;
+      System.out.println( luckyNums[0] );
+      System.out.println( luckyNums[5] );
+      System.out.println( luckyNums[3] ); // prints 0
+      // System.out.println( luckyNums[45] ); // index 45 is out of bounds!
 
+      // Create an array with values using an INITIALIZER LIST
+      String[] table = { "Mia", "Ethan", "Aaniyah" };
+      System.out.println( table[1] ); // gets the SECOND value in array
+      System.out.println( table[0] ); // gets the FIRST value (index 0)
+      // ONLY use INITIALIZER LIST shortcut when first declaring array
+      // i.e. we can't do: apCompSci = {"name1", "name2"}
+
+      // The .length ATTRIBUTE gets the NUMBER OF ITEMS in array
+      int lengthOfTableArray = table.length;
+      System.out.println( lengthOfTableArray ); // 3 items 
+      // To get the LAST INDEX use: arrayName.length - 1
+      int finalIndex = table.length - 1;
+      System.out.println( finalIndex ); // 2 is final index
+      // To get the LAST ITEM use: arrayName[arrayName.length - 1]
+      System.out.println( table[finalIndex] ); // table[2] -> "Aaniyah"
+      System.out.println( table[table.length - 1 ] ); // same as above
 
 
    } // end main METHOD
